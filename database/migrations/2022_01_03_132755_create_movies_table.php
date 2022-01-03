e3cc94987b305c9759f5bfa168a287cbbf77e683
KeLong <kelong23042001@gmail.com>
2022-01-03 20:30:41 +0700

    create database

---
+++ database/migrations/2022_01_03_132755_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',50);
+            $table->longText('description',255);
+            $table->integer('status');
+            $table->string('image',25);
+            $table->integer('category_id');
+            $table->integer('genre_id');
+            $table->integer('country_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
